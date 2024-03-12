d384de86861b180f0d173427e26ef6c06af1588a
DevMansuri <nadimmansu@gmail.com>
2024-03-13 00:34:01 +0530

    modified : props with functional component

--- helloworld/src/components/Greet.js
+++ helloworld/src/components/Greet.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Greet() {
-    return <h1>Hello Nadim</h1>
+function Greet(props) {
+    return (
+        <div>
+        <h1>Hello {props.name}</h1>
+        {props.children}
+        </div>
+    ) 
 
 }
 
