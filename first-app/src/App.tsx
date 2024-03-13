a497528d10e3d899c079dd5d54834000b47035ce
DevMansuri <nadimmansu@gmail.com>
2024-03-14 03:13:59 +0530

    modified : passing function in component

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -2,9 +2,12 @@ import ListGroup from "./components/ListGroups";
 
 function App() {
   const items = ["indore","dewas","bhopal","ujjain"];
+  const handleSelectItem = (item:string) => {
+    console.log(item);
+  }
   return (
     <div>
-      <ListGroup items={items} heading="Cities"></ListGroup>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}></ListGroup>
     </div>
   );
 }
