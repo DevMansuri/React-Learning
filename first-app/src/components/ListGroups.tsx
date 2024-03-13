5201bcdcd664dd3ecd36d60e51fc7ebd3be0f4ac
DevMansuri <nadimmansu@gmail.com>
2024-03-14 02:27:15 +0530

    Modified : Add Event handler

--- first-app/src/components/ListGroups.tsx
+++ first-app/src/components/ListGroups.tsx
@@ -1,13 +1,18 @@
+import { MouseEvent } from "react";
 function ListGroup() {
 
     const items = ["indore","dewas","bhopal","ujjain"];
+    // Event handler
+    const handleClick = (event: MouseEvent) => console.log(event);
     
     return (
         <>
         <h1>List</h1>
         <ul className="list-group">
             {items.map((item) => (
-            <li className="list-group-item" onClick={()=> {console.log(item)}}>
+            <li className="list-group-item"
+                key={item}
+                onClick={handleClick}>
                 {item}
 
             </li>
