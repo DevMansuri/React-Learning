0e2c1c1d16c748ade034e6c646bbb2f4ef7d847b
DevMansuri <nadimmansu@gmail.com>
2024-03-14 03:01:59 +0530

    modified : use props

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,9 +1,10 @@
 import ListGroup from "./components/ListGroups";
 
 function App() {
+  const items = ["indore","dewas","bhopal","ujjain"];
   return (
     <div>
-      <ListGroup></ListGroup>
+      <ListGroup items={items} heading="Cities"></ListGroup>
     </div>
   );
 }
