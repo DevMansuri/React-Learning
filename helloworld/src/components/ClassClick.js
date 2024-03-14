c99bf090056a2741d8ea74e30fda9592ef377f1c
DevMansuri <nadimmansu@gmail.com>
2024-03-14 23:30:01 +0530

    Add component : event handling with function and class

---
+++ helloworld/src/components/ClassClick.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class ClassClick extends Component {
+    clickHandler() {
+        console.log("Clicked again");
+    }
+  render() {
+    return (
+      <div><button onClick={this.clickHandler}>Click me</button></div>
+    )
+  }
+}
+
+export default ClassClick
---
+++ helloworld/src/components/FunctionClick.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FunctionClick() {
+ function   clickHandler() {
+     console.log("Clicked")
+    }
+  return (
+    <div><button onClick={clickHandler}>click</button></div>
+  )}
+
+export default FunctionClick
