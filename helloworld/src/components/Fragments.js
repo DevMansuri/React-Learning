8cde39a9b55a33602ef8f749fb6710a4b24fae52
DevMansuri <nadimmansu@gmail.com>
2024-03-28 03:40:57 +0530

    Add new Component : Fragment Example

---
+++ helloworld/src/components/Fragments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Fragments() {
+  return (
+    <React.Fragment>
+        <h1>Hello</h1>
+        <p>i am Fragment</p>
+    </React.Fragment>
+  )
+}
+export default Fragments
