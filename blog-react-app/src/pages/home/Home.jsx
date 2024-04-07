417a2fdd0dcc763fb5cbba4dba7963ee9b50d7e0
DevMansuri <nadimmansu@gmail.com>
2024-04-07 15:28:41 +0530

    modified

--- blog-react-app/src/pages/home/Home.jsx
+++ blog-react-app/src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import Header from '../../components/header/Header'
+import './home.css'
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/SideBar";
 
 const Home = () => {
   return (
-    <div>
-        <Header></Header>
-        Homepage
+    <>
+    <Header></Header>
+    <div className="home">
+      <Posts />
+      <Sidebar />
     </div>
+  </>
+
   )
 }
 
