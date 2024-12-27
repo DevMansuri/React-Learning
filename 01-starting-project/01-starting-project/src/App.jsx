52fc47bf89c0732b98be00e9d03feef9e15359b7
vishwas-grid <vishwaskumar@grid.suuchi.com>
2024-12-27 23:01:48 +0530

    Update file : add new function and using props

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -1,20 +1,32 @@
-const reactDescriptions = ["Fundamental","Crucial","Core"];
+import reactImg from './assets/react-core-concepts.png'
+import { CORE_CONCEPTS } from "./data";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 function genRandom(max) {
-   return Math.floor(Math.random() * (max +1));
+  return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
   const description = reactDescriptions[genRandom(2)];
-  return(
+  return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
-  )  
+  );
+}
+function CoreConcepts({image,title,description}) {
+  return (
+  <li>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+  );
 }
 
 function App() {
@@ -22,7 +34,15 @@ function App() {
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
