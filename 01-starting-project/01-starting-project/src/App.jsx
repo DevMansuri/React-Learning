792c74b672e4ee175886e327c800d4d5d6feb5a9
vishwas-grid <vishwaskumar@grid.suuchi.com>
2025-01-08 13:57:09 +0530

    Update file

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -1,33 +1,6 @@
-import reactImg from './assets/react-core-concepts.png'
+import Header from "./components/header/Header";
+import CoreConcepts from "./components/coreconcepts/CoreConcepts";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-function genRandom(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandom(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-function CoreConcepts({image,title,description}) {
-  return (
-  <li>
-    <img src={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
-  );
-}
 
 function App() {
   return (
